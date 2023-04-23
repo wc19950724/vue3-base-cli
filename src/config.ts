df9b4cef2cfe6ee7aa90a9ca0dc59f8311f86ed9
王超 <wangchao1.vendor@senseauto.com>
2023-04-23 18:38:12 +0800

    gitPath

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
   /** 仓库地址 */
-  gitPath: "https://gitee.com/wc19950724/vue3-tdesign-ts.git",
+  gitPath: "https://github.com/wc19950724/vue3-base.git",
   /** 项目启动脚本 */
   startCommand: "start",
 };
